dd03f4262529a42becbc4742ef3c2b49e10cda85
Kenyblvk <ezeilokenechukwu@gmail.com>
2025-03-29 14:30:52 +0000

    Update adderFunction_test.go

--- adderFunction_test.go
+++ adderFunction_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func Test_addNumbers(t *testing.T) {
+func TestaddNumbers(t *testing.T) {
 	result := addNumbers(2, 3)
 	if result != 5 {
 		t.Error("incorrect result: expected 5, got", result)
